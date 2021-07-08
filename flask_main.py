629a16b23344c6ffc67376196f63138e5e80cd1c
Will Hall <will@44ld.co.uk>
2021-07-08 16:40:24 +0100

    Added flash new

---
+++ flask_main.py
@@ -0,0 +1,15 @@
+#---------------------------------------------------------------------#
+# File: /home/will/GitHub Repos/PirateGame/flask_main.py
+# Project: /home/will/GitHub Repos/PirateGame
+# Created Date: Thursday, July 8th 2021, 4:38:56 pm
+# Description: Main flask wedserver root.
+# Author: Will Hall
+# Copyright (c) 2021 Lime Parallelogram
+# -----
+# Last Modified: Thu Jul 08 2021
+# Modified By: Will Hall
+# -----
+# HISTORY:
+# Date      	By	Comments
+# ----------	---	---------------------------------------------------------
+#---------------------------------------------------------------------#
