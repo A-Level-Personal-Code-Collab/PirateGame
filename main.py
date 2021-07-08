7e68b485ba36e38bd68ec0f529df24cc9ae057cd
Will Hall <will@44ld.co.uk>
2021-07-08 16:26:00 +0100

    created main file

---
+++ main.py
@@ -0,0 +1,16 @@
+#---------------------------------------------------------------------#
+# File: /home/will/GitHub Repos/PirateGame/main.py
+# Project: /home/will/GitHub Repos/PirateGame
+# Created Date: Thursday, July 8th 2021, 4:25:26 pm
+# Description: 
+# Author: Will Hall
+# Copyright (c) 2021 Lime Parallelogram
+# -----
+# Last Modified: Thu Jul 08 2021
+# Modified By: Will Hall
+# -----
+# HISTORY:
+# Date      	By	Comments
+# ----------	---	---------------------------------------------------------
+#---------------------------------------------------------------------#
+#Imports modules
