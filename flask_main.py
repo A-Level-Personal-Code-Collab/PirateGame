ae9987ae58f19c6b17bc36af016d80f29ee90720
Will Hall <will@44ld.co.uk>
2021-07-09 22:46:52 +0100

    Added Client & Server Code to draw basic grid

--- flask_main.py
+++ flask_main.py
@@ -15,13 +15,59 @@
 # 2021-07-08	WH	Added very basic flask server structure
 #---------------------------------------------------------------------#
 #Imports modules 
-from flask import Flask, render_template
+from flask import Flask, render_template, Markup
 
 app = Flask(__name__)
 
+#=========================================================#
+#Slave subs
+
+#---------------#
+#Builds the HTML syntax to draw the game grid - makes the grid easily exapandable + avoid typing repetative HTML code
+def buildGrid():
+    #Local Constants
+    CLASS_NAME = "gridSquare" #The name of the class that all td objects will share
+
+    #Grid variable will acumulate the HTML table
+    grid = "<table id=\"tbl_CoordinatesGrid\">"
+
+    collums = [" ","A","B","C","D","E","F"] #The lables on the X-Axis
+    rows = list(range(1,7)) #The Y-axis lables
+
+    #Adds heading row
+    grid += "<tr id=\"trw_gridHeading\">"
+    for l in collums:
+        grid += f"<td class=\"{CLASS_NAME}\" idng dynamic html=\"tdt_gridLabelCol{l}\">{l}</td>"
+    grid += "</tr>"
+
+    #Adds rest of grid
+    for row in rows:
+        grid += "<tr>"
+        for col in collums:
+            if col == " ":
+                grid += f"<td class=\"{CLASS_NAME}\" id=\"tdt_gridLabelCol{row}\">{row}</td>"
+            else:
+                grid += f"<td class=\"{CLASS_NAME}\" id=\"tdt_grid{col}{row}\"></td>"
+        grid += "</tr>"
+        
+    grid += "</table>"
+
+    #Return finnished grid as Markdown and not plain text
+    return Markup(grid)
+
+#=========================================================#
+#URL routes
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route("/game")
+def game_sheet():
+    gridHTML = buildGrid()
+    print(gridHTML)
+    return render_template("game_sheet.html", grid = gridHTML)
+
+#=========================================================#
+#Main app execution
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ static/javascript/game_sheet.js
@@ -0,0 +1,15 @@
+/**
+ * File: /home/will/GitHub Repos/PirateGame/assets/javascript/game_sheet.js
+ * Project: Digital Pirate Game
+ * Created Date: Friday, July 9th 2021, 9:29:43 pm
+ * Author: Will Hall
+ * -----
+ * Last Modified: Fri Jul 09 2021
+ * Modified By: Will Hall
+ * -----
+ * Copyright (c) 2021 Lime Parallelogram
+ * ------------------------------------
+ * Javascript will save your soul!
+ */
+
+
