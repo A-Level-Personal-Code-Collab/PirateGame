86e1139d871e932e4ce69e2db8f36dfbe30858e3
Will Hall <will@44ld.co.uk>
2021-07-29 11:27:31 +0100

    Money Items Duplicate & Combine Correctly

--- static/javascript/game_sheet.js
+++ static/javascript/game_sheet.js
@@ -4,7 +4,7 @@
  * Created Date: Friday, July 9th 2021, 9:29:43 pm
  * Author: Will Hall
  * -----
- * Last Modified: Tue Jul 20 2021
+ * Last Modified: Thu Jul 29 2021
  * Modified By: Will Hall
  * -----
  * Copyright (c) 2021 Lime Parallelogram
@@ -13,6 +13,7 @@
  * HISTORY:
  * Date      	By	Comments
  * ----------	---	---------------------------------------------------------
+ * 2021-07-29	WH	Money items now operate correctly in terms of duplicating themselves if they are allowed and re-combining when they are placed back into the bank
  * 2021-07-20	WH	Dropping may now occur in the bank section and items will return to the correct collum based on whether they are money or other items
  * 2021-07-20	WH	Modified drop script so items can oly be dropped in empty cells.
  * 2021-07-11	WH	Created required javascript to allow for draggable grid elements
@@ -21,7 +22,8 @@
 /*Get elements from the page*/
  const draggables = document.querySelectorAll('.gridItems')
  const containers = document.querySelectorAll('.dragReceptical')
- 
+ const moneyItemBank = document.getElementById("div_moneyItems")
+
  /*Contant parameters*/
 const maxMoneys = 
 {
@@ -31,28 +33,96 @@ const maxMoneys =
   M200 : 18
 }
 
+/*#=========================================================#*/
+/*Program variables*/
+/*Keeps track of how many of each money type have been used*/
  var numMoneys = 
  {
-   M5000 : 0,
-   M1000 : 0,
-   M500 : 0,
-   M200 : 0
+   M5000 : 1,
+   M1000 : 1,
+   M500 : 1,
+   M200 : 1
  }
 
+ /*#=========================================================#*/
+ /*User subs*/
+ /*Drag start event listener*/
+ function dragstartevent(draggable, event) {
+  event.dataTransfer.setDragImage(draggable, -99999, -99999); /*Move ghost images away to very far away from screen so you can't see them*/
+  /*UNFINNISHED - Attemps to create new money item if more are available*/
+  if(draggable.classList.contains("itmMoney") && draggable.parentNode.id == "div_moneyItems") {
+    const classLists = draggable.classList
+    var moneyDuplicate = false
+    var afterItem = null /*The previous next most valuable money item that the duplicated item will be inserted before*/
+    if (classLists.contains("mon5000") && numMoneys["M5000"] < maxMoneys["M5000"]) {
+      numMoneys["M5000"] ++
+      moneyDuplicate = true
+      afterItem = moneyItemBank.querySelector(".mon1000")
+    } else if (classLists.contains("mon1000") && numMoneys["M1000"] < maxMoneys["M1000"]) {
+      numMoneys["M1000"] ++
+      moneyDuplicate = true
+      afterItem = moneyItemBank.querySelector(".mon500")
+    } else if (classLists.contains("mon500") && numMoneys["M500"] < maxMoneys["M500"]) {
+      numMoneys["M500"] ++
+      moneyDuplicate = true
+      afterItem = moneyItemBank.querySelector(".mon200")
+    } else if (classLists.contains("mon200") && numMoneys["M200"] < maxMoneys["M200"]) {
+      numMoneys["M200"] ++
+      moneyDuplicate = true
+    }
+
+    if (moneyDuplicate) {
+      var newDraggable = draggable.cloneNode(true)
+      newDraggable.addEventListener('dragstart', (event) => {dragstartevent(newDraggable,event)})
+      newDraggable.addEventListener('dragend', () => {dragendevent(newDraggable)})
+      console.log(afterItem)
+      if (afterItem == null)
+      {
+        console.log("Should insert to end")
+        moneyItemBank.appendChild(newDraggable)
+      }
+      else {
+        console.log("Should insert after")
+        moneyItemBank.insertBefore(newDraggable,afterItem)
+      }
+    }
+    
+  }
+  draggable.classList.add('dragging') /*Add css class that styles the dragging item*/
+ }
+
+ function dragendevent(draggable) {
+  if (draggable.parentNode.id == "div_moneyItems")
+  {
+    if (draggable.classList.contains("mon5000") && moneyItemBank.querySelector(".mon5000:not(.dragging)") != null)
+    {
+      numMoneys["M5000"] --
+      draggable.remove()
+    } else if (draggable.classList.contains("mon1000") && moneyItemBank.querySelector(".mon1000:not(.dragging)") != null)
+    {
+      numMoneys["M1000"] --
+      draggable.remove()
+    } else if (draggable.classList.contains("mon500")  && moneyItemBank.querySelector(".mon500:not(.dragging)") != null)
+    {
+      numMoneys["M500"] --
+      draggable.remove()
+    } else if (draggable.classList.contains("mon200")  && moneyItemBank.querySelector(".mon200:not(.dragging)") != null)
+    {
+      numMoneys["M200"] --
+      draggable.remove()
+    }
+  }
+  draggable.classList.remove('dragging') /*Remove dragging css class*/
+
+}
  /*Add event listeners*/
  draggables.forEach(draggable => {
    draggable.addEventListener('dragstart', (event) => {
-     event.dataTransfer.setDragImage(draggable, -99999, -99999); /*Move ghost images away to very far away from screen so you can't see them*/
-     /*UNFINNISHED - Attemps to create new money item if more are available*/
-     if(draggable.classList.contains("itmMoney")) {
-       const moneyItemBank = document.getElementById("div_moneyItems")
-     }
-     
-     draggable.classList.add('dragging') /*Add css class that styles the dragging item*/
+     dragstartevent(draggable,event)
    })
  
    draggable.addEventListener('dragend', () => {
-     draggable.classList.remove('dragging') /*Remove dragging css class*/
+     dragendevent(draggable)
    })
  })
  
@@ -68,9 +138,25 @@ const maxMoneys =
      if (container.id == "div_itemBank") {
        const draggable = document.querySelector(".dragging")
        /*If item is money, handle it differently*/
-       if (draggable.classList.contains("itmMoney"))
+       var classList = draggable.classList
+       if (classList.contains("itmMoney"))
        {
-         document.getElementById("div_moneyItems").appendChild(draggable)
+          const classLists = draggable.classList
+          var afterItem = null /*The previous next most valuable money item that the duplicated item will be inserted before*/
+          if (classLists.contains("mon5000")) {
+            afterItem = moneyItemBank.querySelector(".mon1000")
+          } else if (classLists.contains("mon1000")) {
+            afterItem = moneyItemBank.querySelector(".mon500")
+          } else if (classLists.contains("mon500")) {
+            afterItem = moneyItemBank.querySelector(".mon200")
+          } 
+          if (afterItem == null)
+          {
+            moneyItemBank.appendChild(draggable)
+          } else
+          {
+            moneyItemBank.insertBefore(draggable,afterItem)
+          }
        } else{
          document.getElementById("div_specialItems").appendChild(draggable)
        }
