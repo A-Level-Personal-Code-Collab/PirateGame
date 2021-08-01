07c3642be676e3328e3c2830ff2fae696f42a741
Theocat321 <adamo.mark25@gmail.com>
2021-08-01 16:23:46 +0100

    Changed Play game page to include the Join game and new game function

---
+++ static/javascript/pre_game.js
@@ -0,0 +1,14 @@
+/**
+ * File: c:\Adam\coding\1. VC Coding projects\PirateGame\static\javascript\pre_game.js
+ * Project: c:\Adam\coding\1. VC Coding projects\PirateGame\static\javascript
+ * Created Date: Sunday, August 1st 2021, 3:48:42 pm
+ * Author: Adam O'Neill
+ * -----
+ * Last Modified: Sun Aug 01 2021
+ * Modified By: Adam O'Neill
+ * -----
+ * Copyright (c) 2021 Adam O'Neill
+ * ------------------------------------
+ * Javascript will save your soul!
+ */
+
