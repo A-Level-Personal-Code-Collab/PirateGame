a5f596ab840e942f3d9cb1a6e237b8d5bb0c02c8
Theocat321 <adamo.mark25@gmail.com>
2021-08-02 13:31:13 +0100

    New Game sliders added

---
+++ static/javascript/new_game.js
@@ -0,0 +1,14 @@
+/**
+ * File: c:\Adam\coding\1. VC Coding projects\PirateGame\static\javascript\new_game.js
+ * Project: c:\Adam\coding\1. VC Coding projects\PirateGame\static\javascript
+ * Created Date: Monday, August 2nd 2021, 1:24:07 pm
+ * Author: Adam O'Neill
+ * -----
+ * Last Modified: Mon Aug 02 2021
+ * Modified By: Adam O'Neill
+ * -----
+ * Copyright (c) 2021 Adam O'Neill
+ * ------------------------------------
+ * Javascript will save your soul!
+ */
+
