da8d58bcd5d5328687322e2effccc83bffc90af6
Theocat321 <adamo.mark25@gmail.com>
2021-08-02 14:55:13 +0100

    Settings page update

--- static/javascript/new_game.js
+++ static/javascript/new_game.js
@@ -12,3 +12,22 @@
  * Javascript will save your soul!
  */
 
+const disablable = document.querySelectorAll(".disablable")
+const custom_game_mode_button = document.getElementById("custom_game_mode_button")
+
+function onload() {
+    custom_game_mode_button.addEventListener("change",disable)
+}
+
+function disable() {
+    var button_status = custom_game_mode_button.checked /* boolean variable */ 
+    console.log(button_status)
+    if (button_status == true) {
+        disablable.forEach(i => {i.classList.remove("disablable"); i.disabled = false; console.log(i)})
+    }
+    else     {
+        disablable.forEach(i => {i.classList.add("disablable"); i.disable = true; console.log(i)})
+    }
+}
+
+onload()
