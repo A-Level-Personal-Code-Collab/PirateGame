446b258985ede7f6b52c517a1cc3cd4f673e9a63
Theocat321 <adamo.mark25@gmail.com>
2021-08-10 12:28:52 +0100

    Game-play lobby file setup

---
+++ static/javascript/lobby.js
@@ -0,0 +1,15 @@
+/**
+ * File: c:\Adam\coding\1. VC Coding projects\PirateGame\static\javascript\lobby.js
+ * Project: c:\Adam\coding\1. VC Coding projects\PirateGame\static\javascript
+ * Created Date: Tuesday, August 10th 2021, 12:28:04 pm
+ * Author: Adam O'Neill
+ * -----
+ * Last Modified: Tue Aug 10 2021
+ * Modified By: Adam O'Neill
+ * -----
+ * Copyright (c) 2021 Adam O'Neill
+ * ------------------------------------
+ * Javascript will save your soul!
+ */
+
+
