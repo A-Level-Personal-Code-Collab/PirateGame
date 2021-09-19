8a51fa4a600ccfc594bb66ee4df7bfff1a9ba4e5
Ollie B <ollieburroughs05@gmail.com>
2021-09-19 21:17:29 +0100

    started support for results page

---
+++ static/javascript/results.js
@@ -0,0 +1,18 @@
+/**
+ * File: c:\Users\ollie\OneDrive\Desktop\Documents\GitHub Repos\PirateGame\static\javascript\results.js
+ * Project: c:\Users\ollie\OneDrive\Desktop\Documents\GitHub Repos\PirateGame\static\javascript
+ * Created Date: Sunday, September 19th 2021, 8:35:59 pm
+ * Author: Ollie Burroughs
+ * -----
+ * Last Modified: Sun Sep 19 2021
+ * Modified By: Ollie Burroughs
+ * -----
+ * Copyright (c) 2021 SchmetCorp.
+ * ------------------------------------
+ * -----
+ * HISTORY:
+ * Date      	By	Comments
+ * ----------	---	---------------------------------------------------------
+ */
+
+
