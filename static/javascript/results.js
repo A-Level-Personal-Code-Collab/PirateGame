ca23f74ca7a5a5eafa4f5f15bada0ccc9b83ca98
Ollie B <ollieburroughs05@gmail.com>
2021-09-26 18:08:55 +0100

    Podiums (WIP)

--- static/javascript/results.js
+++ static/javascript/results.js
@@ -4,7 +4,7 @@
  * Created Date: Sunday, September 19th 2021, 8:35:59 pm
  * Author: Ollie Burroughs
  * -----
- * Last Modified: Sun Sep 19 2021
+ * Last Modified: Sun Sep 26 2021
  * Modified By: Ollie Burroughs
  * -----
  * Copyright (c) 2021 SchmetCorp.
@@ -14,5 +14,15 @@
  * Date      	By	Comments
  * ----------	---	---------------------------------------------------------
  */
-
-
+const firsttxt = document.getElementById("firsttxt")
+const secondtxt = document.getElementById("secondtxt")
+const thirdtxt = document.getElementById("thirdtxt")
+fitty('#firsttxt')
+fitty('#secondtxt')
+fitty('#thirdtxt')
+console.log(top3str)
+var top3dict = JSON.parse(top3str)
+var top3users = Object.keys(top3dict)
+firsttxt.innerHTML = top3users[0]
+secondtxt.innerHTML = top3users[1]
+thirdtxt.innerHTML = top3users[2]
