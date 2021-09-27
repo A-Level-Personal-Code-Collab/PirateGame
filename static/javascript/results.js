cdef0deb7b71df712767e167472e7c6e8ac85d24
Will Hall <will@limeparallelogram.uk>
2021-09-27 21:24:12 +0100

    Sort Podium users by money (Issue #111)

--- static/javascript/results.js
+++ static/javascript/results.js
@@ -1,11 +1,11 @@
 /**
- * File: c:\Users\ollie\OneDrive\Desktop\Documents\GitHub Repos\PirateGame\static\javascript\results.js
- * Project: c:\Users\ollie\OneDrive\Desktop\Documents\GitHub Repos\PirateGame\static\javascript
+ * File: https://github.com/A-Level-Personal-Code-Collab/PirateGame/static/javascript/results.js
+ * Project: https://github.com/A-Level-Personal-Code-Collab/PirateGame
  * Created Date: Sunday, September 19th 2021, 8:35:59 pm
  * Author: Ollie Burroughs
  * -----
- * Last Modified: Sun Sep 26 2021
- * Modified By: Ollie Burroughs
+ * Last Modified: Mon Sep 27 2021
+ * Modified By: Will Hall
  * -----
  * Copyright (c) 2021 SchmetCorp.
  * ------------------------------------
@@ -13,16 +13,35 @@
  * HISTORY:
  * Date      	By	Comments
  * ----------	---	---------------------------------------------------------
+ * 2021-09-27	WH	Sorts top 3 user properly
+ * 2021-09-26	OB	Updates podium positions with player's name
+ * 2021-09-26	OB	Added fitty reference to resize existing text
  */
 const firsttxt = document.getElementById("firsttxt")
 const secondtxt = document.getElementById("secondtxt")
 const thirdtxt = document.getElementById("thirdtxt")
-fitty('#firsttxt')
-fitty('#secondtxt')
-fitty('#thirdtxt')
-console.log(top3str)
-var top3dict = JSON.parse(top3str)
-var top3users = Object.keys(top3dict)
-firsttxt.innerHTML = top3users[0]
-secondtxt.innerHTML = top3users[1]
-thirdtxt.innerHTML = top3users[2]
+
+function on_load()
+{
+    //Parse dictionary from python and sort the top 3 in array form
+    var top3dict = JSON.parse(top3str)
+    var sortedArray = Object.keys(top3dict).map(function(key) { return [key, top3dict[key]]});
+    sortedArray.sort(function(first,second) {return second[1] - first[1]})
+
+    //#//
+    //Update podium positions
+    firsttxt.innerHTML = sortedArray[0][0]
+    secondtxt.innerHTML = sortedArray[1][0]
+    thirdtxt.innerHTML = sortedArray[2][0]
+
+    /*---------------*/
+    //Run resize function to fit winner's names on podium
+    fitty('#firsttxt')
+    fitty('#secondtxt')
+    fitty('#thirdtxt')
+
+}
+
+//=========================================================//
+//^ Execute OnLoad function ^//
+on_load();
