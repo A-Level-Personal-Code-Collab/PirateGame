47a425ef585782daf3aead60ebb26367389721ff
Will Hall <will@44ld.co.uk>
2021-11-11 10:11:21 +0000

    Further work on deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,13 @@
 # A dedicated script to run before deploying a version of the pirate game.
 
 #Replace all socketio addresses
-grep -RiIl 'http://localhost:8001/' | xargs sed -i 's/"http://localhost:8001/"/"http://play.pirategame.live/websockets/"/g'
+grep -RiIl 'http://localhost:8001/' | xargs sed -i 's,"http://localhost:8001/","http://play.pirategame.live/websockets/",g'
 
 #Replace all website
-grep -RiIl 'http://localhost:8000/' | xargs sed -i 's/"http://localhost:8000/"/"http://play.pirategame.live/"/g'
+grep -RiIl 'http://localhost:8000/' | xargs sed -i 's,"http://localhost:8000/","http://play.pirategame.live/",g'
 
 #Replace all database password data
-grep -RiIl 'local-only' | xargs sed -i 's/local-only/$(cat /secrets/database)/g'
+grep -RiIl 'local-only' | xargs sed -i 's,local-only,$(cat /secrets/database),g'
 
 #Build dockers
 docker-compose -f docker-compose.prod.yml up -d --build
