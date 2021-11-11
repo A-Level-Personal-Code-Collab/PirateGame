48ccadb1309dd797cd53dbe491191cecc02fb9af
Will Hall <will@44ld.co.uk>
2021-11-11 09:26:53 +0000

    Further work on deployment script

--- deploy.sh
+++ deploy.sh
@@ -2,18 +2,16 @@
 # A dedicated script to run before deploying a version of the pirate game.
 
 #Replace all socketio addresses
-find app/ -type f -exec sed -i 's/"http://localhost:8001/"/"http://play.pirategame.live/websockets/"/g' {} \;
+grep -RiIl 'http://localhost:8001/' | xargs sed -i 's/"http://localhost:8001/"/"http://play.pirategame.live/websockets/"/g'
 
 #Replace all website
-find app/ -type f -exec sed -i 's/"http://localhost:8000/"/"http://play.pirategame.live/"/g' {} \;
+grep -RiIl 'http://localhost:8000/' | xargs sed -i 's/"http://localhost:8000/"/"http://play.pirategame.live/"/g'
 
 #Replace all database password data
-find app/ -type f -exec sed -i 's/local-only/$(cat /secrets/database)/g' {} \;
+grep -RiIl 'local-only' | xargs sed -i 's/local-only/$(cat /secrets/database)/g'
 
 #Build dockers
-docker stop $(docker container ls -aq)
-docker contianer prune -f
-docker-compose up -d -f docker-compose.prod.yml --build
+docker-compose -f docker-compose.prod.yml up -d --build
 
 #Copy static content
 mkdir /var/www
