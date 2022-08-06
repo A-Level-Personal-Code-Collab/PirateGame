852ec45c50c0ee27c4d0926f20886431c19eecd1
Will Hall <will@44ld.co.uk>
2022-08-06 18:48:49 +0100

    Creating game now increments statistics

--- django/pirategame/game/views.py
+++ django/pirategame/game/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework.throttling import ScopedRateThrottle
 
 from .serializers import GameSerializer
+from status.models import Statistic
 
 class CreateGameView(APIView):
     # Client game creation limited at 2/min to limit possibility of silliness
@@ -14,5 +15,11 @@ class CreateGameView(APIView):
         serializer = GameSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
+
+            # Increment total number of games
+            stat_total_game = Statistic.objects.get(stat_name="total_games")
+            stat_total_game.stat_value += 1
+            Statistic.save(stat_total_game)
+            
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- django/pirategame/status/models.py
+++ django/pirategame/status/models.py
@@ -4,3 +4,6 @@ class Statistic(models.Model):
     """Used to track global game statistics"""
     stat_name = models.CharField(primary_key=True,max_length=31)
     stat_value = models.IntegerField()
+
+    def __str__(self):
+        return self.stat_name
